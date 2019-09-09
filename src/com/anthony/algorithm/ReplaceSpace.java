b82f50b438716e6e6aeea2201669e676dc423a3e
Anthony-Lu <18539656510@163.com>
2019-09-10 00:22:14 +0800

    add replaceSpace solution

---
+++ src/com/anthony/algorithm/ReplaceSpace.java
@@ -0,0 +1,32 @@
+package com.anthony.algorithm;
+
+/**
+ *题目描述:
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+ * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ * Created by Anthony on 2019/9/10 0:16
+ */
+public class ReplaceSpace {
+
+    public static String replaceSpace(String str){
+        if(str == null || str.length() == 0){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+        for(int i = 0;i < len;i++){
+            char c = str.charAt(i);
+            if(c == ' '){
+                sb.append("%20");
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We Are Happy"));
+    }
+}
