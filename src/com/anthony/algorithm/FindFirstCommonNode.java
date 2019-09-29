83eb5948ec79f5f84fd9f72baeca02755b3667a2
Anthony-Lu <18539656510@163.com>
2019-09-29 23:45:14 +0800

    add findFirstCommonNode

---
+++ src/com/anthony/algorithm/FindFirstCommonNode.java
@@ -0,0 +1,36 @@
+package com.anthony.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 题目描述：输入两个单向链表，找出它们的第一个公共结点
+ * Created by Anthony on 2019/9/29 23:39
+ */
+public class FindFirstCommonNode {
+    static class Node {
+        Node next;
+        int val;
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public Node solution(Node head1, Node head2) {
+        Node node1 = head1;
+        Node node2 = head2;
+        Map<Node, Integer> map = new HashMap<>();
+        while (node1 != null) {
+            map.put(node1, node1.val);
+            node1 = node1.next;
+        }
+
+        while (node2 != null) {
+            if (map.containsKey(node2)) {
+                return node2;
+            }
+            node2 = node2.next;
+        }
+        return null;
+    }
+}
