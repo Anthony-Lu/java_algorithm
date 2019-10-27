1d7c7676d1e2cd58a0ec37dd2ec0e71e95ad72ff
Anthony-Lu <18539656510@163.com>
2019-10-27 23:52:39 +0800

    add invertBinaryTree

---
+++ src/com/anthony/algorithm/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+package com.anthony.algorithm;
+
+/**
+ * Created by Anthony on 2019/10/27 23:39
+ */
+public class InvertBinaryTree  {
+    static class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void invertBinaryTree(TreeNode root){
+        if(root == null){
+            return;
+        }
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right = node;
+        invertBinaryTree(root.left);
+        invertBinaryTree(root.right);
+    }
+}
